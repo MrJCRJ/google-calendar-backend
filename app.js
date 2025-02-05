0990f845d6b44a70fb54a2a33253bf8232d6ebb9
Jose Cicero <mr.jociriju@gmail.com>
2025-02-05 17:03:57 -0300

    Adiciona suporte a CORS e configura o Vercel para o aplicativo

--- app.js
+++ app.js
@@ -2,16 +2,19 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const routes = require('./routes');
+const cors = require('cors');
 
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+app.use(cors());
+
 
 // Usar as rotas definidas
 app.use('/', routes);
 
 // Iniciar o servidor
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
